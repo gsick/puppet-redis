73b4edf103e7adac1b0936b8d4aff745aa13042e
gsick <gsick@emailvision.com>
2014-03-21 13:36:27 +0100

    initial commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -69,44 +69,43 @@ class redis(
   }
 
   file { "init file":
-    name    => "/etc/init.d/redis_${port}"
+    name    => "/etc/init.d/redis_${port}",
     owner   => root,
     group   => root,
     mode    => 755,
-    content => template("${module_name}/redis_init_script.tpl"),
-    require => Exec['install redis'],
+    content => template("${module_name}/redis_init_script.tpl.erb"),
+    notify  => Service['redis'],
   }
 
-  #if $overwrite_default_conf {
-    exec { 'copy default conf file':
-      cwd => "${tmp}/redis-${version}",
-      command => "cp redis.conf ${conf_dir}/${port}.conf",
-      path => '/bin:/usr/bin',
-      creates => "${conf_dir}/${port}.conf",
-      require => [Exec['install redis'], File['conf dir']],
+  exec { 'copy default conf file':
+    cwd     => "${tmp}/redis-${version}",
+    command => "cp redis.conf ${conf_dir}/${port}.conf",
+    path    => '/bin:/usr/bin',
+    creates => "${conf_dir}/${port}.conf",
+    require => [Exec['install redis'], File['conf dir']],
     }
-  #} else {
-#    file { 'create conf file':
-#      name => "${port}.conf",
-#      path => "${conf_dir}",
-#      ensure => present,
-#      require => Exec['install redis'],
-#    }
-#  }
 
   $conf_tmp = merge($conf, {port => $port, pidfile => $pidfile, logfile => $logfile, dir => $dir})
 
   if (!empty($conf_tmp)) {
     $conf_tmp.each |$key, $value| {
       file_line { "conf_${key}":
-        path => "${conf_dir}/${port}.conf",
-        line => "${key} ${value}",
-        match => "^(${key}\s).*$",
+        path    => "${conf_dir}/${port}.conf",
+        line    => "${key} ${value}",
+        match   => "^(${key}\s).*$",
         require => Exec['copy default conf file'],
+        notify  => Service['redis'],
       }
     }
   }
 
-
+  service { 'redis':
+    name => "redis_${port}",
+    enable => true,
+    ensure => running,
+    hasrestart => true,
+    hasstatus => false,
+    require => [Exec['install redis'], File['data dir']],
+  }
 
 }
