2997f57beedc4abf0f6f16168f5cddb733dbb9e3
gsick <gsick@emailvision.com>
2014-03-27 10:15:57 +0100

    initial commit

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -4,6 +4,7 @@ define redis::instance(
   $servername = $name, 
   $conf = {}, 
   $sentinel = false,
+  $default_template = true,
 ) {
 
   # It's more programmatically, but I don't want a fuck*** template
@@ -11,7 +12,11 @@ define redis::instance(
   # More chance to have auto-compatibility with future version
   # and it's just fun sometimes
 
+  # check parameters
+  validate_string($servername)
   validate_hash($conf)
+  validate_bool($sentinel)
+  validate_bool($default_template)
 
   # default value if not set
   if (empty($conf) or $conf[port] == '') {
@@ -40,19 +45,33 @@ define redis::instance(
 
   $conf_tmp = merge($conf, {port => $port, pidfile => $pidfile, logfile => $logfile, dir => $dir})
 
-  # Select the good template
-  if ($sentinel) {
-    $default_conf_file = 'sentinel.conf'
-  } else {
-    $default_conf_file = 'redis.conf'
+  if($default_template) {
+
+    # select the good template
+    if ($sentinel) {
+      $default_conf_file = 'sentinel.conf'
+    } else {
+      $default_conf_file = 'redis.conf'
+    }
+
+    # copy redis.conf or sentinel.conf
+    exec { "copy default conf file ${servername}":
+      cwd     => "${redis::tmp}/redis-${redis::version}",
+      command => "cp ${default_conf_file} ${redis::conf_dir}/${port}.conf",
+      path    => '/bin:/usr/bin',
+      creates => "${redis::conf_dir}/${port}.conf",
+      require => [Exec['install redis'], File['conf dir']],
+      notify  => File["conf file ${servername}"],
+    }
   }
 
-  # copy redis.conf or sentinel.conf
-  exec { "copy default conf file ${servername}":
-    cwd     => "${redis::tmp}/redis-${redis::version}",
-    command => "cp ${default_conf_file} ${redis::conf_dir}/${port}.conf",
-    path    => '/bin:/usr/bin',
-    creates => "${redis::conf_dir}/${port}.conf",
+  # create an empty file if $default_template == false
+  file { "conf file ${servername}":
+    ensure  => file,
+    path    => "${redis::conf_dir}/${port}.conf",
+    owner   => root,
+    group   => root,
+    mode    => 644,
     require => [Exec['install redis'], File['conf dir']],
   }
 
@@ -62,7 +81,7 @@ define redis::instance(
   }
 
   file { "init file ${servername}":
-    ensure  => 'file',
+    ensure  => file,
     name    => "/etc/init.d/redis_${port}",
     owner   => root,
     group   => root,
@@ -74,29 +93,40 @@ define redis::instance(
   # override properties
   $conf_tmp.each |$key, $value| {
 
-    $regex = "^(#\\s)?(${key}\\s)\
+    # TODO, clean this mess
+    # maybe replace by slice function but need puppet 3.5
+    $key_arr = split($key, '#')
+    $size = size($key_arr)
+    if($size == 2) {
+      $final_key = $key_arr[1]
+    } else {
+      $final_key = $key_arr[0]
+    }
+
+    # magic regex
+    $regex = "^(#\\s)?(${final_key}\\s)\
 (((?!and)(?!192.168.1.100\\s10.0.0.1)\
 (?!is\\sset)\
 [A-Za-z0-9\\._\\-\"/\\s]+)\
 |(<master-password>)|(<masterip>\\s<masterport>)\
 |(<bytes>))$"
 
-    file_line { "conf_${servername}_${key}":
+    file_line { "conf_${servername}_${final_key}":
       path    => "${redis::conf_dir}/${port}.conf",
-      line    => "${key} ${value}",
+      line    => $size ? { 2 => "# ${final_key} ${value}", default => "${final_key} ${value}" },
       match   => $regex,
-      require => Exec["copy default conf file ${servername}"],
+      require => File["conf file ${servername}"],
       notify  => Service["redis ${servername}"],
     }
   }
 
   service { "redis ${servername}":
-    name => "redis_${port}",
-    enable => true,
-    ensure => running,
+    name       => "redis_${port}",
+    enable     => true,
+    ensure     => running,
     hasrestart => true,
-    hasstatus => false,
-    status => "/bin/service redis_${port} status | grep --quiet \"Redis is running\"",
-    require => [Exec['install redis'], File["data dir ${servername}"]],
+    hasstatus  => false,
+    status     => "/bin/service redis_${port} status | grep --quiet \"Redis is running\"",
+    require    => [Exec['install redis'], File["data dir ${servername}"]],
   }
 }
